b336018f497343688b6a0f058d894c934f266b6f
samson <loubel.samson.20@usjr.edu.ph>
2021-04-27 19:13:35 +0800

    Welcome screen UI code

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      title: 'Messaging App',
+      debugShowCheckedModeBanner: false,
       home: WelcomeScreen(),
     );
   }
--- lib/screens/welcomeScreen.dart
+++ lib/screens/welcomeScreen.dart
@@ -5,9 +5,61 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Welcome to Edssenger \nmessaging app"),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 70.0,
+                  child: Text(
+                    "Welcome to Edssenger \nmessaging app",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 100.0,
+                  child: Text(
+                    "Chat with family and \nfriends now!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Theme.of(context)
+                          .textTheme
+                          .bodyText1
+                          .color
+                          .withOpacity(0.65),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  width: 250.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadiusDirectional.circular(30.0),
+                      color: Colors.grey),
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text("Login"),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  width: 250.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadiusDirectional.circular(30.0),
+                      color: Colors.grey),
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text("Register"),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
