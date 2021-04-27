269870b43337345b2dfaff953e6e605bbb0de5fb
samson <loubel.samson.20@usjr.edu.ph>
2021-04-27 14:07:25 +0800

    added home property in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:msg_app/screens/welcomeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
+      home: WelcomeScreen(),
     );
   }
 }
